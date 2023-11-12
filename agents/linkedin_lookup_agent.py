2068e62584dcfa5558e8dcdcd62be386c1810cdb
Joe Gresock <jgresock@gmail.com>
2023-11-12 10:20:52 -0500

    Added LinkedIn processing

---
+++ agents/linkedin_lookup_agent.py
@@ -0,0 +1,30 @@
+from langchain.prompts import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+
+from langchain.agents import initialize_agent, Tool, AgentType
+
+from tools.tools import get_profile_url
+
+
+def lookup(name: str) -> str:
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    template = """given the full name {name_of_person}, I want you to get me a link to their LinkedIn profile page.
+                  Please only include the URL itself in your answer, not surrounding text."""
+
+    tools_for_agent = [
+        Tool(
+            name="Crawl Google 4 linkedin profile page",
+            func=get_profile_url,
+            description="useful for when you need to get the LinkedIn Page URL",
+        )
+    ]
+
+    agent = initialize_agent(
+        tools_for_agent, llm, AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
+    )
+    prompt_template = PromptTemplate(
+        template=template, input_variables=["name_of_person"]
+    )
+
+    linkedin_profile_url = agent.run(prompt_template.format_prompt(name_of_person=name))
+    return linkedin_profile_url
